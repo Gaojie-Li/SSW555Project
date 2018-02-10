8fb27e738ae20d297c5a4de3104ac391f3930e91
Gaojie-Li <i.am.hya.li@gmail.com>
2018-02-10 11:26:28 -0500

    add some comments and questions

--- ProcessResult.py
+++ ProcessResult.py
@@ -2,18 +2,33 @@ import os
 from prettytable import PrettyTable
 from collections import defaultdict
 
-TRANS_TAGS = {'DATE': Name}
+TRANS_TAGS = {'DATE': 'Date'}
+
 
 def parse_line(line):
     items = line.lstrip('<-- ').split('|')
-    return (int(items[0]), items[1], items[2], items[3] if len(items) > 3 else None)
+    return (items[0], items[1], items[2], items[3] if len(items) > 3 else None)
 
+
+# read level 2
 def get_data_of_property(readable, i):
+    cur_item = defaultdict(str)
+
     while i < len(readable):
         line = readable[i]
         level, tag, valid, args = parse_line(line)
 
-        if 
+        # invalid cases
+        if valid == 'N':
+            continue
+        if level == '1':
+            break
+
+        cur_item[tag] = args
+
+    return cur_item, i
+
+# read level 1
 
 
 def get_properties(readable, i):
@@ -21,19 +36,22 @@ def get_properties(readable, i):
 
     while i < len(readable):
         line = readable[i]
+        # 1 NAME Zheng /Li/
         level, tag, valid, args = parse_line(line)
 
-        if 0 == level:
+        if '0' == level:
             break
 
         i += 1
 
-        if 1 != level or 'N' == valid:
+        if '1' != level or 'N' == valid:
             continue
 
         dd[tag], i = get_data_of_property(readable, i)
 
     return dd, i
+
+# read level 0
 
 
 def process_result(input_path, output_path):
@@ -44,32 +62,33 @@ def process_result(input_path, output_path):
 
     readable = open(file).readlines()
 
+    # key: id
     individual_dict = defaultdict(defaultdict(str))
     family_dict = defaultdict(defaultdict(str))
 
     i = 0
+    # read all individual data
     while i < len(readable):
         line = readable[i]
         if line.startswith('-->'):
             continue
 
         level, tag, valid, args = parse_line(line)
+
+        if tag == 'FAM':
+            break
+        # if not valid
         if 'N' == valid:
             continue
 
+        # if not INDI or FAM
         if not (0 == level and 'INDI' == tag):
             continue
-        
+
         indi_id = args.strip('@')
         individual_dict[indi_id], i = get_properties(readable, i)
 
-
-
-
-        individual_dict
-
-
-
+    # read all family data
 
     individual_table = PrettyTable(field_names=[
                                    'ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse'])
